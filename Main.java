3dccf383fcec40713c64837239319c211df35dfe
Denisius664 <denismir423@gmail.com>
2020-03-06 12:52:49 +1100

    Change Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
  public static void main(String[] args){
-  System.out.println("Hello world");
+  System.out.println("Hello lord");
+  Scanner scan = new Scanner(System.in);
+  System.out.print("Enter a number: ");
+  int user = scan.nextInt();
  }
 } 
